f042a6df11dbd91562bf8dfd7f7c4668aa9c9f98
BretasArthur1 <arthur.bretas@sou.inteli.edu.br>
2025-10-26 19:54:35 -0300

    feat: add buyback instructions

---
+++ programs/programs/programs/src/errors.rs
@@ -0,0 +1,37 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Buyback is currently disabled")]
+    BuybackDisabled,
+    
+    #[msg("Treasury has insufficient balance")]
+    InsufficientTreasuryBalance,
+    
+    #[msg("Invalid buyback amount")]
+    InvalidAmount,
+    
+    #[msg("Invalid treasury account")]
+    InvalidTreasury,
+    
+    #[msg("Invalid collection mint")]
+    InvalidCollectionMint,
+    
+    #[msg("User does not own the NFT")]
+    InvalidNFTOwner,
+    
+    #[msg("Token account mint does not match NFT mint")]
+    InvalidNFTMint,
+    
+    #[msg("Invalid NFT amount (must be 1)")]
+    InvalidNFTAmount,
+    
+    #[msg("Math operation overflow")]
+    MathOverflow,
+    
+    #[msg("Minimum treasury balance too high")]
+    InvalidMinBalance,
+    
+    #[msg("Unauthorized: Only authority can perform this action")]
+    Unauthorized,
+}
---
+++ programs/programs/programs/src/instructions/mod.rs
@@ -0,0 +1,7 @@
+mod initialize;
+mod execute;
+mod toggle_buyback;
+
+pub use initialize::*;
+pub use execute::*;
+pub use toggle_buyback::*;
